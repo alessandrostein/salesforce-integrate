5468f66d969a10ec49260966e668f0d1830e1dc3
alessandrostein <alessandro@alessandrostein.com>
2014-08-04 12:01:37 -0300

    Partial Controller

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -3,29 +3,34 @@ require 'rd_person'
 class LeadsController < ApplicationController
 
 	def index
-		@lead = Lead.index
-	end
-
-	def show
-		
+		render = 'index'
 	end
 
 	def new
-		@lead = 'new'
+		render = 'new'
 	end
 
 	def create
-	
-		@lead = lead_params
-	
+		@name = params[:lead][:name]
+		@last_name = params[:lead][:last_name]
+		@email = params[:lead][:email]
+		@company = params[:lead][:company]
+		@job_title = params[:lead][:job_title]
+		@phone = params[:lead][:phone]
+		@website = params[:lead][:website]
+
+
+	    @people = Person.new(@name, @last_name, @email, @company, @job_title, @phone, @website)
+
+    	@client = SalesforceClient.new('', '', '',
+                                  '',
+                                  '')
+    	@client.create(@people)
+
 		respond_to do |format|
 			format.html {render "index"}
-			format.xml {render xml: @lead}
+			format.xml {render xml: @client}
 		end
 	end
-
-	def lead_params
-      	params.require(:lead).permit(:name, :last_name, :email, :company, :job_title, :phone, :website)
-    end
 
 end
--- app/models/Lead.rb
+++ app/models/Lead.rb
@@ -1,3 +1,2 @@
 class Lead
-	attr_accessor :name, :last_name, :email, :company, :job_title, :phone, :website
 end
