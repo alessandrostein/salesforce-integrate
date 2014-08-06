cb1998cdd9ce319291f67b9e04b459c544a6ec22
alessandrostein <alessandro@alessandrostein.com>
2014-08-05 23:30:37 -0300

    Add gem Resque and configure

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'spring',        group: :development
 
 gem 'rd_person', '~> 0.0.4'
 
+gem 'resque'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
---
+++ config/initializers/resque.rb
@@ -0,0 +1,3 @@
+resque_config = YAML.load_file("#{Rails.root}/config/resque.yml")
+Resque.redis = resque_config[Rails.env]
+Resque.redis.namespace = "resque:#{Rails.application.class.parent_name}"
