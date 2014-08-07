e0803a754c9a0a0c2b2d2414eaab778d99143875
alessandrostein <alessandro@alessandrostein.com>
2014-08-07 16:46:21 -0300

    Update resque

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   resources :leads
-
-require 'resque/server'
- 
-  mount Resque::Server => "/internal/resque"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
