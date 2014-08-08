ce6ba886f5fe0041b14be624eafb93d82f4c186e
alessandrostein <alessandro@alessandrostein.com>
2014-08-08 13:46:57 -0300

    Update Lead Controller

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -52,7 +52,7 @@ class LeadsController < ApplicationController
           format.json { render json: @client.errors, status: :unprocessable_entity }
         end
       rescue
-        format.html { render :new }
+        format.html { render :new, notice: 'Error the connect SalesforceClient' }
         format.json { render json: Resque.errors, status: :unprocessable_entity }
       end
     end
@@ -94,14 +94,14 @@ class LeadsController < ApplicationController
     end    
 
     def config_rd_person
-      salesforce_username = ENV["SALESFORCE_USERNAME"]
-      salesforce_password =  ENV["SALESFORCE_PASSWORD"]
-      salesforce_security_token = ENV["SALESFORCE_SECURITY_TOKEN"]
-      salesforce_client_id = ENV["SALESFORCE_CLIENT_ID"] 
-      salesforce_client_secret = ENV["SALESFORCE_CLIENT_SECRET"]
+      @salesforce_username = ENV["SALESFORCE_USERNAME"]
+      @salesforce_password =  ENV["SALESFORCE_PASSWORD"]
+      @salesforce_security_token = ENV["SALESFORCE_SECURITY_TOKEN"]
+      @salesforce_client_id = ENV["SALESFORCE_CLIENT_ID"] 
+      @salesforce_client_secret = ENV["SALESFORCE_CLIENT_SECRET"]
   
-      @client = SalesforceClient.new(salesforce_username, salesforce_password, salesforce_security_token,
-                                     salesforce_client_id, salesforce_client_secret)
+      @client = SalesforceClient.new(@salesforce_username, @salesforce_password, @salesforce_security_token,
+                                     @salesforce_client_id, @salesforce_client_secret)
     end
 
     def set_lead_rd_person
